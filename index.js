4596a1f695c15c4688e1456488b99bd83a6e9d33
Bryan Jay Zacarias <zacarbr@oocl.com>
2022-08-18 14:29:41 +0800

    ZACARBR String Js

--- index.js
+++ index.js
@@ -2,12 +2,28 @@
 var name = 'hello';
 // TODO write your code here...
 
+name = name.toUpperCase();
+
+//To view the answer
+console.log(name);
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
 
+function toUpperCaseFirstLetter(str) {
+   var splitStr = str.toLowerCase().split(' ');
+   for (var i = 0; i < splitStr.length; i++) {
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+   }
+  
+   return splitStr.join(' '); 
+}
+//To view answer
+console.log(toUpperCaseFirstLetter(sentence));
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+console.log(money.substring(1, 3));
+
